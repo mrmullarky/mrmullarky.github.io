17e3e4585746247ca181272c618bcc0eca0b3204
mrmullarky <conormullarky@gmail.com>
2019-02-19 12:34:02 +0000

    Revised Website.
    Added opaque navbar for mobile.
    Refactored CSS and removed redundant divs

--- js/scripting.js
+++ js/scripting.js
@@ -4,21 +4,38 @@ $(document).ready(function(){
     var div3 = $('#projects').offset().top;
     var div4 = $('#connect').offset().top;
 
-    $(document).scroll(function(){
-        var scrollPos = $(document).scrollTop();
-        if(scrollPos >= div1 && scrollPos < div2){
-            $('#navbar').css("background-color", "#04a343");
-        } else if(scrollPos >= div2 && scrollPos < div3){
-            $('#navbar').css("background-color", "#FFC0CB");
-        } else if(scrollPos >= div3 && scrollPos < div4){
-            $('#navbar').css("background-color", "#fff");
-        } else if(scrollPos >= div4) {
-            $('#navbar').css("background-color", '#f48f42');
-        }
-    });
+    if(window.matchMedia('(max-width: 800px)').matches){
+        $(document).scroll(function(){
+            var scrollPos = $(document).scrollTop();
+            if(scrollPos >= div1 && scrollPos < div2){
+                $('#navbar').css("background-color", "rgb(106, 181, 101, 1)");
+            } else if(scrollPos >= div2 && scrollPos < div3){
+                $('#navbar').css("background-color", "rgb(122, 211, 116, 1)");
+            } else if(scrollPos >= div3 && scrollPos < div4){
+                $('#navbar').css("background-color", "rgb(255, 255, 255, 1)");
+            } else if(scrollPos >= div4) {
+                $('#navbar').css("background-color", '#f48f42');
+            }
+        });
+    } else {
+        $(document).scroll(function(){
+            var scrollPos = $(document).scrollTop();
+            if(scrollPos >= div1 && scrollPos < div2){
+                $('#navbar').css("background-color", "rgb(106, 181, 101, 0.5)");
+            } else if(scrollPos >= div2 && scrollPos < div3){
+                $('#navbar').css("background-color", "rgb(122, 211, 116, 0.5)");
+            } else if(scrollPos >= div3 && scrollPos < div4){
+                $('#navbar').css("background-color", "rgb(255, 255, 255, 0.5)");
+            } else if(scrollPos >= div4) {
+                $('#navbar').css("background-color", '#f48f42');
+            }
+        });
+    }
+
+
 });
 
-function myFunc() {
+loadNav = () => {
     var x = document.getElementById("navbar");
     if (x.className === "navbar") {
       x.className += " responsive";
